55f433a40993a223f6a19163152997f90c3849c2
hcooli <609795643.com>
2018-11-20 11:25:51 +0800

    update run.py

--- run.py
+++ run.py
@@ -3,19 +3,22 @@ import time
 from multiprocessing import Pool
 import os
 import numpy as np
+
+
 def tuning_washing_small():
     start = time.time()
     p = Pool(11)
     print('Parent process %s.' % os.getpid())
     for Ep in np.arange(2.0, 6.0, 1.0):
         for Eb in np.arange(0, 3.2, 0.2):
-            p.apply_async(washing_small, args=(1, Ep, Eb, Ep*4))
+            p.apply_async(washing_small, args=(1, Ep, Eb, Ep * 3))
     print('Waiting for all subprocesses done...')
     p.close()
     p.join()
     print('All subprocesses done.')
     end = time.time()
     print('Tasks runs %0.2f seconds.' % (end - start))
+
 
 def draw_pictures():
     start = time.time()
@@ -33,6 +36,7 @@ def draw_pictures():
     end = time.time()
     print('Tasks runs %0.2f seconds.' % (end - start))
 
+
 def tuning_step_heating():
     start = time.time()
     p = Pool(11)
@@ -46,6 +50,7 @@ def tuning_step_heating():
     print('All subprocesses done.')
     end = time.time()
     print('Tasks runs %0.2f seconds.' % (end - start))
+
 
 if __name__ == '__main__':
 
